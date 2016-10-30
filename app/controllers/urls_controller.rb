7b1b56f0f20bdca74bddc558170ad6ffdbb3e58e
Russell Christensen <russellchristensen@me.com>
2016-10-30 14:19:48 -0400

    adds simple form and functioning url shortener

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,4 +1,5 @@
 class UrlsController < ApplicationController
+  before_action :find_url, only: [:show, :shortened]
 
   def index
     @url = Url.new
@@ -8,29 +9,34 @@ class UrlsController < ApplicationController
     redirect_to @url.sanitized_url
   end
 
+  def shortened
+    @allurl = Url.all
+    @url = Url.find_by_short_url(params[:short_url])
+  end
+
   def create
     @url = Url.new(url_params)
     @url.sanitize
-  if @url.new_url?
-    if @url.save
-      redirect_to shortened_path(@url.short_url)
+    if @url.new_url?
+      if @url.save
+        redirect_to shortened_path(@url.short_url)
+      else
+        flash[:error] = "Check the error below:"
+        render 'index'
+      end
     else
-      flash[:error] = "Error"
-      render 'index'
+      flash[:notice] = "A short link for this URL is already in our database"
+      redirect_to shortened_path(@url.find_duplicate.short_url)
     end
-  else
-    flash[:notice] = "A shortened URL has already been created."
   end
-end
 
-private
+  private
 
-def find_url
-  @url = Url.find_by_short_url(params:[:short_url])
-end
+  def find_url
+    @url = Url.find_by_short_url(params[:short_url])
+  end
 
-def url_params
-  params.require(:url).permit(:original_url)
-end
-
+  def url_params
+    params.require(:url).permit(:original_url)
+  end
 end
