82d03eded64e17e0381441b34c9849afde896e77
Russell Christensen <russellchristensen@me.com>
2017-02-07 20:01:12 -0500

    updated url helper for og:title

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -1,18 +1,14 @@
 module UrlsHelper
+#This only works if the document has OpenGraph title. Need to add additional tags, validation that
+#a tag is present, and error messaging
 
     def link(url)
       resp = HTTParty.get(url)
       doc = Nokogiri::HTML(resp.body)
 
-      sitename    = doc.at('meta[property="og:site_name"]')['content']
-      title       = doc.at('meta[property="og:title"]')['content']
-      description = doc.at('meta[property="og:description"]')['content']
-      image       = doc.at('meta[property="og:image"]')['content']
-      if title = doc.at('meta[property="og:title"]')['content']
-        return title
-      elsif sitename || title || description || image
-      return title
-    end
+
+    return doc.at('meta[property="og:title"]')['content']
+
 
   end
 end
